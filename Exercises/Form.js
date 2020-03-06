50542484d5574c2103365ae2b0326d11145b1bdb
nahed david <nahed.david@gmail.com>
2020-03-05 19:53:02 -0500

    Form

---
+++ Exercises/Form.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let app = express()
+const bodyParser = require('body-parser')
+const url = require('url')
+const cookieParser = require('cookie-parser')
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    if (req.query) {
+        if (req.query.pw === '0') {
+          res.render('template', { invalid: true })
+        } else {
+          res.render('template')
+        }
+      } else {
+        res.render('template')
+      }
+    })
+  app.post('/formsubmit', (req, res) => {
+    console.log('TCL: req', req.body)
+    const { password } = req.body
+    // Revisar si password viene vacio
+    if (!password) {
+      res.redirect(
+        303,
+        url.format({
+          pathname: '/',
+          query: {
+            pw: 0
+          }
+        })
+      )
+    } else {
+      res.render('template', { sent: "true" })
+    }
+})  
+app.listen(9000, () => console.log('Listening on port 9000!'))
